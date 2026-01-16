fa2f67925e20528036d31eaa177e3a6394c0fd56
joeybail96 <joey.bail@utah.edu>
2026-01-16 00:07:18 -0700

    reorganizing social media

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -100,6 +100,12 @@ export default function HomePage() {
 
             {/* Bottom icons */}
             <div className="pt-4 flex justify-center gap-3">
+              <SocialIcon href="mailto:joey.bail@gmail.com" label="Email">
+                <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor" className="text-[#EA4335]">
+                  <path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
+                </svg>
+              </SocialIcon>
+
               <SocialIcon
                 href="https://www.linkedin.com/in/josephbail2018/"
                 label="LinkedIn"
@@ -112,12 +118,6 @@ export default function HomePage() {
               <SocialIcon href="https://github.com/joeybail96" label="GitHub">
                 <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor" className="text-black">
                   <path d="M12 .5C5.65.5.5 5.65.5 12c0 5.1 3.3 9.4 7.9 10.9.6.1.8-.3.8-.6v-2.2c-3.2.7-3.9-1.5-3.9-1.5-.5-1.2-1.1-1.6-1.1-1.6-.9-.6.1-.6.1-.6 1 .1 1.6 1 1.6 1 .9 1.6 2.5 1.1 3.1.8.1-.7.4-1.1.7-1.4-2.6-.3-5.4-1.3-5.4-5.9 0-1.3.5-2.3 1.1-3.2-.1-.3-.5-1.5.1-3.2 0 0 .9-.3 3.3 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.7.2 2.9.1 3.2.7.9 1.1 2 1.1 3.2 0 4.6-2.8 5.6-5.5 5.9.5.4.8 1.1.8 2.2v3.3c0 .3.2.7.8.6 4.6-1.5 7.9-5.8 7.9-10.9C23.5 5.65 18.35.5 12 .5z" />
-                </svg>
-              </SocialIcon>
-
-              <SocialIcon href="mailto:joey.bail@gmail.com" label="Email">
-                <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor" className="text-[#EA4335]">
-                  <path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
                 </svg>
               </SocialIcon>
 
