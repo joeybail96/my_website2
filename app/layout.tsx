ddfe31f31fc9fbb7255afacabf259f44ea2350b5
joeybail96 <joey.bail@utah.edu>
2026-02-03 16:42:56 -0700

    adding analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} bg-zinc-50 dark:bg-black`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
